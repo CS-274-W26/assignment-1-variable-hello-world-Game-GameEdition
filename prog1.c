e938d00678bbc45d2e903026a4911377237d5d77
Game-GameEdition <pleajustdoit@gmail.com>
2026-01-17 19:03:26 -0800

    assgn1 done! :)

--- prog1.c
+++ prog1.c
@@ -1,11 +1,25 @@
 #include <stdio.h> // Necessary for printf()
-
-// TODO Other includes
-
+#include <stdlib.h> // we need for random
+#include <time.h> // to seed random
 
 int main() {
 	// The main function is the entrypoint of the program. When the program
 	// is executed, it will automatically start here.
-	
 	// TODO Complete the program
+	srand(time(NULL)); // seed based on time
+	int myrand;
+	myrand = rand() % 11; // modulo 10 excludes 10
+	if (myrand>=0 && myrand <=4) {
+		printf("eat more beef, kick less cats\n");
+	}
+	else if (myrand>=5 && myrand<=9) {
+		printf("FRODO LIVES \n");
+	}
+	else if (10==myrand){
+		printf("Larn is the best roguelike \n");
+	}
+
+	printf("the random number was: %d\n", myrand);
+
+	return 0;
 }
